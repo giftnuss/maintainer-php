2ab812371f87c2208f92df12d1ac21140d4a8f6f
Sebastian B. Knapp <news@young-workers.de>
2022-02-04 03:44:54 -0500

    start setup

--- bin/setup.bash
+++ bin/setup.bash
@@ -1,13 +1,17 @@
 #!/usr/bin/env bash
 
-set -x
+#set -x
 set -e
 
-if [ ! -d upstream ]; then
-    git clone https://gitlab.com/foodsharing-dev/foodsharing.git upstream
-fi
-
 if [ ! -d working ]; then
-    git clone https://github.com/giftnuss/foodsharing.git working
+    git clone https://github.com/thephpleague/climate.git working
 fi
 
+(
+    cd working
+    git fetch --all --tags --quiet
+    git branch -a | grep -w v2.6.1 >/dev/null
+    if [ $? -ne 0 ]; then
+       git checkout tags/2.6.1 -b v2.6.1
+    fi
+)
